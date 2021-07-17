4720c086ea5e6a00734e06f8c7ef193cf45072a5
Leon Nothegger <nothegger.leon@gmail.com>
2021-07-17 19:30:31 +0200

    Logic added

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <status :winner="winner.player" :player="player"/>
     <div class="container">
       <field v-for="(field, index) in fields" :key="index" :value="field" v-on:click="change(index)"/>
   </div>
@@ -8,16 +9,20 @@
 
 <script>
     import field from './Field.vue';
+    import status from './Status.vue';
+    import getWinner from '../winner'
 
 export default {
     name: "board",
     components: { 
-        field
+        field,
+        status
     },
   data(){
       return{
           player:"X",
-          fields: Array(9).fill(null)
+          fields: Array(9).fill(null),
+          isDrawGame: false,
       }
     },
     methods: {
@@ -28,6 +33,11 @@ export default {
       this.$set(this.fields, index, this.player);
         this.player = this.player === "X" ? "O": "X";
         }
+    },
+    computed: {
+      winner(){
+        return getWinner(this.fields)
+      }
     }
 };
 </script>
